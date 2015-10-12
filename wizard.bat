472cfaf1f797970ce4fbd788178437f90f6d1e87
James Wright <james-jw@users.noreply.github.com>
2015-10-12 14:21:38 -0600

    Renamed supporting query files.

--- wizard.bat
+++ wizard.bat
@@ -3,24 +3,24 @@
 @echo off
 setlocal enableextensions enabledelayedexpansion
 set /a "x = 1"
-set /p source="Enter the source directory: "
+set /p source="Enter the source directory to hunt: "
 set /p db="Enter the database name to create the index in: "
 set /p skip="Enter the skip expression: "
 set /p pages="Enter how many pages it will take: "
 set /p sizes="Enter trigram sizes, seperated by commas: "
 cls
-start /b /wait basex -q "(db:create('%db%'), db:output('Created database %db% succesfully!'))"
+start /b /wait basex -q "(db:create('%db%'), db:output('Created Hunt database %db% succesfully!'))"
 :while1
 if %x% leq %pages% (
-    start /b /wait basex -bsource=%source% -bdb="%db%" -bskip=%skip% -bpage=%x% -bsizes="%sizes%" jw-use-index.xq
+    start /b /wait basex -bsource=%source% -bdb="%db%" -bskip=%skip% -bpage=%x% -bsizes="%sizes%" hunt-directory.xq
     
     set /a "x = x + 1"
     goto :while1
 )
 
 start /b /wait basex -q "(db:optimize('%db%'))"
-echo Indexing Trigrams
-start /b /wait basex -bdb="%db%" jw-index-database.xq
+echo Hunting Trigrams
+start /b /wait basex -bdb="%db%" index-hunt-database.xq
 start /b /wait basex -q "(db:optimize('%db%'))"
-echo Indexing %source% into database %db% Complete!
+echo Hunting %source% into database %db% Complete!
 endlocal
